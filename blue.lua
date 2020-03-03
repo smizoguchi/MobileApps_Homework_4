7e4b0265f5d0ca3f9b1c03a242bf5b86253edb5d
smizoguchi <mizoguchy@gmail.com>
2020-03-03 00:03:42 -0600

    Update blue.lua

--- blue.lua
+++ blue.lua
@@ -3,7 +3,9 @@ local Blue = Block:new({HP=2,tag='box',color='blue'});
 
 
 function Blue:spawn()
-	self.shape= display.newRect(self.xPos, self.yPos,display.contentWidth/6,display.contentHeight/12);
+	self.shape= display.newRect(self.xPos, self.yPos,(display.contentWidth-40)/6,display.contentHeight/12);
+	self.shape.anchorX = 0;
+	self.shape.anchorY = 0;
 	self.shape.pp= self;      -- parent pointer to parent object
 	self.shape.tag= self.tag; --“enemy”
 	self.shape:setFillColor(0,0,1);

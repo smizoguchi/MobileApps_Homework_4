c5e7ca204c2cf648954f5dae10d0500d2412af3f
smizoguchi <mizoguchy@gmail.com>
2020-03-03 00:03:49 -0600

    Update grey.lua

--- grey.lua
+++ grey.lua
@@ -1,9 +1,11 @@
 local Block = require("Block");
-local Grey = Block:new({HP=99,tag='box',color='grey'});
+local Grey = Block:new({HP=1,tag='box',color='grey'});
 
 
 function Grey:spawn()
-	self.shape= display.newRect(self.xPos, self.yPos,display.contentWidth/6,display.contentHeight/12);
+	self.shape= display.newRect(self.xPos, self.yPos,(display.contentWidth-40)/6,display.contentHeight/12);
+	self.shape.anchorX = 0;
+	self.shape.anchorY = 0;
 	self.shape.pp= self;      -- parent pointer to parent object
 	self.shape.tag= self.tag; --“enemy”
 	self.shape:setFillColor(.5,.5,.5);

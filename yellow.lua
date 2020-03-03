c54138a85f8fb177f74a553c1dbcd505d7ab4e23
smizoguchi <mizoguchy@gmail.com>
2020-03-03 00:03:52 -0600

    Update yellow.lua

--- yellow.lua
+++ yellow.lua
@@ -5,7 +5,9 @@ local yellow = block:new({HP=1,tag='box',color="yellow"});
 
 
 function yellow:spawn()
-	self.shape= display.newRect(self.xPos, self.yPos,display.contentWidth/6,display.contentHeight/12);
+	self.shape= display.newRect(self.xPos, self.yPos,(display.contentWidth-40)/6,display.contentHeight/12);
+	self.shape.anchorX = 0;
+	self.shape.anchorY = 0;
 	self.shape.pp= self;      -- parent pointer to parent object
 	self.shape.tag= self.tag; --“enemy”
 	self.shape:setFillColor(1,1,0);

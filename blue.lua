48cd1868828149d2e9b285f87c5a4ebad9a82097
smizoguchi <mizoguchy@gmail.com>
2020-03-02 23:50:35 -0600

    Update blue.lua

--- blue.lua
+++ blue.lua
@@ -1,4 +1,13 @@
 local Block = require("Block");
 local Blue = Block:new({HP=2,tag='box',color='blue'});
 
+
+function Blue:spawn()
+	self.shape= display.newRect(self.xPos, self.yPos,display.contentWidth/6,display.contentHeight/12);
+	self.shape.pp= self;      -- parent pointer to parent object
+	self.shape.tag= self.tag; --“enemy”
+	self.shape:setFillColor(0,0,1);
+	physics.addBody(self.shape, "kinematic"); 
+end
+
 return Blue;

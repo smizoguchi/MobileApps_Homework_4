0f57766611fb5118708880279cb3866a3ab4111a
smizoguchi <mizoguchy@gmail.com>
2020-03-02 23:50:42 -0600

    Update yellow.lua

--- yellow.lua
+++ yellow.lua
@@ -2,4 +2,14 @@ local block = require("block")
 
 local yellow = block:new({HP=1,tag='box',color="yellow"});
 
+
+
+function yellow:spawn()
+	self.shape= display.newRect(self.xPos, self.yPos,display.contentWidth/6,display.contentHeight/12);
+	self.shape.pp= self;      -- parent pointer to parent object
+	self.shape.tag= self.tag; --“enemy”
+	self.shape:setFillColor(1,1,0);
+	physics.addBody(self.shape, "kinematic"); 
+end
+
 return yellow;

952d78c663d1a579f8c5e0ac7b95c7a8c85b33af
Nick Dea <36463843+ShadowStorm6@users.noreply.github.com>
2020-03-03 18:11:03 -0600

    Update level_1.lua

--- level_1.lua
+++ level_1.lua
@@ -4,6 +4,11 @@ local blue = require("blue")
 local yellow = require("yellow")
 local grey = require("grey")
 
+local physics = require("physics");
+physics.start();
+
+physics.setGravity(0,0);
+
 local scene = composer.newScene()
  
 -- -----------------------------------------------------------------------------------
@@ -20,19 +25,19 @@ local scene = composer.newScene()
 
 -- create()
 function scene:create( event )
- 
+
     local sceneGroup = self.view;
     
     -- Code here runs when the scene is first created but has not yet appeared on screen
-
-
  
 end
 
  
 -- show()
 function scene:show( event )
- 
+
+
+
     local sceneGroup = self.view
     local phase = event.phase
  
@@ -42,63 +47,6 @@ function scene:show( event )
     elseif ( phase == "did" ) then
         -- Code here runs when the scene is entirely on screen
 
-        local physics = require("physics");
-        physics.start();
-        
-        physics.setGravity(0,0);
-
-        
-        local top = display.newRect(0,100,display.contentWidth, 20);
-        local left = display.newRect(0,0,20, display.contentHeight);
-        local right = display.newRect(display.contentWidth-20,0,20,display.contentHeight);
-        local bottom = display.newRect(0,display.contentHeight-20, display.contentWidth, 20);
-
-        
-        sceneGroup:insert(top);
-        sceneGroup:insert(left);
-        sceneGroup:insert(right);
-        sceneGroup:insert(bottom);
-        top.anchorX= 0;top.anchorY = 0;
-        left.anchorX= 0;left.anchorY = 0;
-        right.anchorX= 0;right.anchorY = 0;
-        bottom.anchorX= 0;bottom.anchorY = 0;
-
-
-        physics.addBody( bottom, "static" );
-        physics.addBody( top, "static" );
-        physics.addBody( left, "static" );
-        physics.addBody( right, "static" );
-
-
-        local paddle = display.newRect(display.contentCenterX, display.contentHeight-100, 200, 20);
-        physics.addBody( paddle, "kinematic");
-        sceneGroup:insert(paddle);
-
-        local function move ( event )
-            if event.phase== "began" then
-                paddle.markX= paddle.x
-            elseif event.phase== "moved" then
-                    local x = (event.x-event.xStart) + paddle.markX;paddle.x= x;
-            end
-        end
-
-        Runtime:addEventListener("touch", move);
-
-        local playerHP = 5;
-        local plan = "Lives: "..playerHP
-        local TxtOpt = 
-            {
-                text = plan,
-                x = display.contentCenterX,
-                y = 50,
-                width = 1080,
-                font = native.systemFontBold,
-                fontSize = 48,
-                align = "center"
-            }
-
-        local endText = display.newText(TxtOpt);
-
         local ball = display.newCircle(display.contentCenterX, display.contentCenterY-50, 20);
         physics.addBody(ball, "dynamic", {bounce=1, radius=20} );
 
@@ -106,24 +54,60 @@ function scene:show( event )
 
         ball:applyForce(0,50,ball.x,ball.y)
 
-            
-        local function cheat(event) 
-            print("in cheat")
-            if (event.phase == "began") then
-                print("phase began")
-                if (event.target.tag == "box") then
-                    print("touching ball")
-                    event.target.pp:hit()
-                end
+        local playerHP = 3;
+        local lifeCount = display.newText(
+        {
+            text = "Lives: "..playerHP,
+            x = display.contentCenterX,
+            y = 50,
+            width = 1080,
+            font = native.systemFontBold,
+            fontSize = 48,
+            align = "center"
+        });
+        sceneGroup:insert(lifeCount);
+
+        local top = display.newRect(0,100,display.contentWidth, 20);
+        local left = display.newRect(0,0,20, display.contentHeight);
+        local right = display.newRect(display.contentWidth-20,0,20,display.contentHeight);
+        local bottom = display.newRect(0,display.contentHeight-20, display.contentWidth, 20);
+    
+        
+        sceneGroup:insert(top);
+        sceneGroup:insert(left);
+        sceneGroup:insert(right);
+        sceneGroup:insert(bottom);
+    
+        top.anchorX= 0;top.anchorY = 0;
+        left.anchorX= 0;left.anchorY = 0;
+        right.anchorX= 0;right.anchorY = 0;
+        bottom.anchorX= 0;bottom.anchorY = 0;
+    
+    
+        physics.addBody( bottom, "static" );
+        physics.addBody( top, "static" );
+        physics.addBody( left, "static" );
+        physics.addBody( right, "static" );
+    
+    
+        local paddle = display.newRect(display.contentCenterX, display.contentHeight-100, 200, 20);
+        physics.addBody( paddle, "kinematic");
+        sceneGroup:insert(paddle);
+        
+    
+        local function move ( event )
+            if event.phase== "began" then
+                paddle.markX= paddle.x
+            elseif event.phase== "moved" then
+                    local x = (event.x-event.xStart) + paddle.markX;paddle.x= x;
             end
         end
-
-
+    
+        Runtime:addEventListener("touch", move);
+        
         local normCount = 0;
         local yCount = 0;
         local gCount = 0;
-        local all = display.newGroup()
-
 
 		for counta = 1,4 do
 			for countb = 1,6 do
@@ -140,36 +124,28 @@ function scene:show( event )
     					if normCount < 18 then
     						normCount = normCount + 1
     						r = block:new({xPos=xpos, yPos=ypos});
-    						r = r:spawn();
-                            all:insert(r);
-                            r:addEventListener("touch", cheat)
+    						r:spawn();
                             spawned =true;
     					end
     				elseif (bType == 2) then
     					if normCount < 18 then
     						normCount = normCount + 1
     						b = blue:new({xPos=xpos, yPos=ypos});
-    						b = b:spawn();
-                            all:insert(b);
-                            b:addEventListener("touch", cheat)
+    						b:spawn();
                             spawned =true;
     					end
     				elseif (bType == 3) then
     					if yCount < 2 then
     						yCount = yCount + 1;
     						y = yellow:new({xPos=xpos, yPos=ypos});
-    						y = y:spawn();
-                            all:insert(y);
-                            y:addEventListener("touch", cheat)
+    						y:spawn();
                             spawned =true;
     					end
     				else
     					if gCount < 4 then
     						gCount = gCount + 1;
     						g = grey:new({xPos=xpos, yPos=ypos});
-    						g = g:spawn();	
-                            all:insert(g);
-                            g:addEventListener("touch", cheat)
+    						g:spawn();	
                             spawned =true;
     					end
     				end
@@ -178,53 +154,38 @@ function scene:show( event )
 		end
 
 
-        local function yellowCollision() 
-            print("hello")
-            for i=1,#all do
-                if all[i].color=="red" then
-                    all[i]:setFillColor(0,0,1)
-                    all[i].HP = 2;
-                elseif all[i].color=="blue" then
-                    all[i].shape:setFillColor(1,0,0)
-                    all[i].HP = 1;
-                end
-            end
-        end
-
-
         local function ballCollision(event)
             if (event.phase== "began") then
                 if (event.other== paddle) then
-                    plan="Lives: "..playerHP;
+                    lifeCount.text="Lives: "..playerHP;
                 elseif (event.other == bottom) then
-                    print("hello")
-                    playerHP = playerHP - 1
-                    plan="Lives: "..playerHP;
+                    playerHP = playerHP - 1;
+                        lifeCount.text="Lives: "..playerHP;
 
-                    if life==0 then
-                        plan = "You're bad at this game."
+                    if playerHP==0 then
+                        lifeCount.text  = "You're bad at this game."
                         ball:removeSelf();
                         ball=nil;
                     end
                 elseif (event.other.tag == 'box') then
                 	if(event.other.color ~= 'grey') then
                         if(event.other.color == 'yellow') then
-                            yellowCollision();
-                        end
                         
+                        end
                         event.other.pp:hit(); 
                     end
                            
                 end 
             end
         end
-
         ball:addEventListener("collision", ballCollision);
-
+    end
+    if(normCount == 0 and yCount == 0 and gCount == 4)then
+        composer.gotoScene("level_2", {effect = "fade", time = 500})
     end
 end
  
- 
+
 -- hide()
 function scene:hide( event )
  
